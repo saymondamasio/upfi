97d430c8af2eb6123d5854bfd467754a3badfcf8
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-06 15:38:00 -0300

    fix: width thumbnail

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
 import {
   Box,
   Heading,
-  Text,
   Image,
   Skeleton,
   SkeletonText,
+  Text,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 
@@ -30,7 +30,7 @@ export function Card({ data, viewImage }: CardProps): JSX.Element {
           src={data.url}
           alt={data.title}
           objectFit="cover"
-          w="max"
+          w="100%"
           h={48}
           borderTopRadius="md"
           onClick={() => viewImage(data.url)}
